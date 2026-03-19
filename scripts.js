780617bb969b1077af2afc06cf291fbda1e8cb57
Pritom Saha <pritomkumarsaha@live.com>
2026-03-19 05:32:11 -0500

    major design and content update

--- scripts.js
+++ scripts.js
@@ -1,34 +1,39 @@
-/*!
-* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
+    // Bootstrap ScrollSpy on the top nav
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            offset: 74,
+            target: '#mainNav',
+            offset: 80,
         });
-    };
+    }
 
-    // Collapse responsive navbar when toggler is visible
+    // Collapse responsive navbar when a nav link is clicked (mobile)
     const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
+    const navLinks = [].slice.call(document.querySelectorAll('#navbarNav .nav-link'));
+    navLinks.forEach(function (navLink) {
+        navLink.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
                 navbarToggler.click();
             }
         });
+    });
+
+    // Abstract toggle chevron icon flip
+    document.querySelectorAll('.abstract-toggle').forEach(function (btn) {
+        const targetId = btn.getAttribute('data-bs-target');
+        const collapseEl = document.querySelector(targetId);
+        if (collapseEl) {
+            collapseEl.addEventListener('show.bs.collapse', function () {
+                const icon = btn.querySelector('i');
+                if (icon) { icon.classList.replace('fa-chevron-down', 'fa-chevron-up'); }
+            });
+            collapseEl.addEventListener('hide.bs.collapse', function () {
+                const icon = btn.querySelector('i');
+                if (icon) { icon.classList.replace('fa-chevron-up', 'fa-chevron-down'); }
+            });
+        }
     });
 
 });
